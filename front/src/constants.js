faa897d9ab0cb541232ec171e0f8e4c09b558c3c
Riftland <fran.mscr@gmail.com>
2023-07-23 00:59:58 +0200

    chore: changed questions list by an object

---
+++ front/src/constants.js
@@ -0,0 +1,35 @@
+export const questions = {
+  food: {
+    text: "¿Que te apetece comer?",
+    options: [
+      { id: "italian", text: "Pizza", isCorrect: true },
+      { id: "american", text: "Burger", isCorrect: true },
+      { id: "spanish", text: "Bocata de Calamares", isCorrect: true },
+    ],
+  },
+  activity: {
+    text: "¿Que estas haciendo en este momento?",
+    options: [
+      { id: "a", text: "Estudiando", isCorrect: true },
+      { id: "b", text: "Trabajando", isCorrect: true },
+      {
+        id: "c",
+        text: "Ninguna de las anteriores, quiero fiesta!!!",
+        isCorrect: true,
+      },
+    ],
+  },
+
+  friends: {
+    text: "¿Cuantos sois?",
+    options: [
+      { id: "1", text: "Estoy solo, no tengo amigos...", isCorrect: true },
+      { id: "2", text: "Yo y mi amig@", isCorrect: true },
+      {
+        id: "3",
+        text: "Un monton!!! no puedo contarlos a todos",
+        isCorrect: true,
+      },
+    ],
+  },
+};
